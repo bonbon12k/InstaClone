4bdc3eecd5856c4ab3b2d059818dd3556c24d43a
salvador-martinez <salvador.martinez@mbible.org>
2016-02-24 15:13:46 -0600

    Friend list with search filter created

--- app/src/main/java/com/dehboxturtle/instaclone/FriendsFragment.java
+++ app/src/main/java/com/dehboxturtle/instaclone/FriendsFragment.java
@@ -1,23 +1,31 @@
 package com.dehboxturtle.instaclone;
 
 
-import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
+import android.widget.*;
 import com.firebase.client.DataSnapshot;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
 import com.firebase.client.ValueEventListener;
 
+import java.io.InputStream;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -25,13 +33,98 @@ import java.util.ArrayList;
  */
 public class FriendsFragment extends Fragment {
     EditText searchField;
-    ListView friends;
+    RecyclerView friends;
     Firebase mFirebaseRef;
     FriendAdapter mAdapter;
     ArrayList<Friend> myFriends;
-
+    ArrayList<Friend> allFriends;
+    RecyclerView.LayoutManager mLayoutMaganager;
     public FriendsFragment() {
         // Required empty public constructor
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        friends = null;
+        searchField = null;
+        mAdapter = null;
+        mFirebaseRef = null;
+        myFriends = null;
+
+        Log.i("friendfrag", "onDestoryView");
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mFirebaseRef = new Firebase(getString(R.string.firebase_root));
+
+        myFriends = new ArrayList<>();
+        allFriends = new ArrayList<>();
+
+        friends = (RecyclerView) getView().findViewById(R.id.friendList);
+        friends.setHasFixedSize(true);
+
+        mLayoutMaganager = new LinearLayoutManager(getContext());
+        friends.setLayoutManager(mLayoutMaganager);
+
+        mAdapter = new FriendAdapter(myFriends);
+
+        friends.setAdapter(mAdapter);
+
+        searchField = (EditText) getView().findViewById(R.id.searchField);
+        searchField.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                List<Friend> temp = filter(allFriends, s.toString());
+                mAdapter.animateTo(temp);
+                friends.scrollToPosition(0);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+
+        Firebase friendlist = mFirebaseRef.child("userdata/" + mFirebaseRef.getAuth().getUid() + "/friends");
+        friendlist.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Iterable<DataSnapshot> uids = dataSnapshot.getChildren();
+                for (DataSnapshot d : uids) {
+                    String uid = d.getKey();
+                    mFirebaseRef.child("users/" + uid).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot) {
+                            Friend item = dataSnapshot.getValue(Friend.class);
+                            item.setUid(dataSnapshot.getKey());
+                            myFriends.add(item);
+                            allFriends.add(item);
+                            mAdapter.notifyDataSetChanged();
+                            Log.i("friend added to list", item.getDisplay_name());
+                        }
+
+                        @Override
+                        public void onCancelled(FirebaseError firebaseError) {
+
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(FirebaseError firebaseError) {
+
+            }
+        });
+
+        Log.i("friendfrag", "OnActivityCreated");
     }
 
     @Override
@@ -45,49 +138,250 @@ public class FriendsFragment extends Fragment {
                         .setAction("Action", null).show();
             }
         });
-        mFirebaseRef = new Firebase(getString(R.string.firebase_root));
-
-        myFriends = new ArrayList<>();
-
-        mAdapter = new FriendAdapter(getContext(), myFriends);
-
-        friends.setAdapter(mAdapter);
-
-        Firebase friendlist = mFirebaseRef.child("userdata/" + mFirebaseRef.getAuth().getUid() + "/friends");
-        friendlist.addValueEventListener(new ValueEventListener() {
-            @Override
-            public void onDataChange(DataSnapshot dataSnapshot) {
-                Iterable<DataSnapshot> uids = dataSnapshot.getChildren();
-                for (DataSnapshot d : uids) {
-                    String uid = d.getKey();
-                    mFirebaseRef.child("users/" + uid).addListenerForSingleValueEvent();
-
-
-                    //TODO: I AM HERE ^
-
-                }
-            }
-
-            @Override
-            public void onCancelled(FirebaseError firebaseError) {
-
-            }
-        })
+        Log.i("friendfrag", "OnCreate");
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        Log.i("friendfrag", "OnCreateView");
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_friends, container, false);
     }
 
-    public class FriendAdapter extends ArrayAdapter<Friend> {
-        public FriendAdapter(Context context, ArrayList<Friend> friends) {
-            super(context, 0, friends);
+    public class FriendAdapter extends RecyclerView.Adapter<FriendAdapter.ViewHolder> {
+        private ArrayList<Friend> dataset;
+
+        public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+            ImageView avatar;
+            TextView name;
+            ProgressBar progress;
+
+            public ViewHolder(View view) {
+                super(view);
+                view.setClickable(true);
+                view.setFocusable(true);
+                avatar = (ImageView) view.findViewById(R.id.avatar);
+                name = (TextView) view.findViewById(R.id.name);
+                progress = (ProgressBar) view.findViewById(R.id.progress);
+                view.setOnClickListener(this);
+            }
+
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Clicked " + name.getText().toString(), Toast.LENGTH_SHORT).show();
+            }
         }
 
+        public FriendAdapter(ArrayList<Friend> friends) {
+            dataset = (ArrayList<Friend>)friends;
+            Log.i("friendfrag", "Made Friend Adapter");
+        }
 
+        @Override
+        public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            View v = LayoutInflater.from(getContext())
+                        .inflate(R.layout.friend_list_item, parent, false);
+            ViewHolder vh = new ViewHolder(v);
+            return vh;
+        }
+
+        @Override
+        public void onBindViewHolder(ViewHolder holder, int position) {
+            final Friend f = myFriends.get(position);
+            holder.name.setText(f.getDisplay_name());
+
+            new AsyncTask<ViewHolder, Void, Bitmap>() {
+                private ViewHolder v;
+
+                @Override
+                protected void onPostExecute(Bitmap bitmap) {
+                    super.onPostExecute(bitmap);
+                    v.progress.setVisibility(View.GONE);
+                    v.avatar.setVisibility(View.VISIBLE);
+                    v.avatar.setImageBitmap(bitmap);
+                }
+
+                @Override
+                protected Bitmap doInBackground(ViewHolder... params) {
+                    v = params[0];
+                    String url = f.getAvatar();
+                    Bitmap image = null;
+                    try {
+                        InputStream in = new URL(url).openStream();
+                        image = BitmapFactory.decodeStream(in);
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
+                    return image;
+                }
+            }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, holder);
+        }
+
+        @Override
+        public int getItemCount() {
+            return myFriends.size();
+        }
+
+        public void setDataset(ArrayList<Friend> data) {
+            dataset = data;
+        }
+
+        public Friend removeItem(int position) {
+            final Friend f = dataset.remove(position);
+            notifyItemRemoved(position);
+            return f;
+        }
+
+        public void addItem(int position, Friend f) {
+            dataset.add(position, f);
+            notifyItemInserted(position);
+        }
+
+        public void moveItem(int fromPosition, int toPosition) {
+            final Friend f = dataset.remove(fromPosition);
+            dataset.add(toPosition, f);
+            notifyItemMoved(fromPosition, toPosition);
+        }
+
+        public void animateTo(List<Friend> fs) {
+            applyAndAnimateRemovals(fs);
+            applyAndAnimateAdditions(fs);
+            applyAndAnimateMovedItems(fs);
+        }
+
+        private void applyAndAnimateRemovals(List<Friend> newfriends) {
+            for (int i = dataset.size()-1; i >=0; i--) {
+                final Friend f = dataset.get(i);
+                if (!newfriends.contains(f)) {
+                    removeItem(i);
+                }
+            }
+        }
+
+        private void applyAndAnimateAdditions(List<Friend> newFriends) {
+            for (int i = 0, count = newFriends.size(); i < count; i++) {
+                final Friend f = newFriends.get(i);
+                if (!dataset.contains(f)) {
+                    addItem(i, f);
+                }
+            }
+        }
+
+        private void applyAndAnimateMovedItems(List<Friend> newFriends) {
+            for (int toPosition = newFriends.size() - 1; toPosition >=0; toPosition--) {
+                final Friend f = newFriends.get(toPosition);
+                final int fromPosition = dataset.indexOf(f);
+                if (fromPosition >= 0 && fromPosition != toPosition) {
+                    moveItem(fromPosition, toPosition);
+                }
+            }
+        }
+    }
+
+    private List<Friend> filter(List<Friend> fl, String query) {
+        query = query.toLowerCase();
+
+        final List<Friend> filteredModelList = new ArrayList<>();
+        for (Friend f : fl) {
+            final String text = f.getDisplay_name().toLowerCase();
+            if (text.contains(query)) {
+                filteredModelList.add(f);
+            }
+        }
+        return filteredModelList;
     }
 
 }
+
+ /*   private class FriendFilter extends Filter {
+
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            FilterResults results = new FilterResults();
+            if (constraint == null || constraint.length() == 0) {
+                results.values = allFriends;
+                results.count = allFriends.size();
+            } else {
+                List<Friend> newFriendsList = new ArrayList<>();
+
+                for (Friend f : myFriends) {
+                    if (f.getDisplay_name().toUpperCase().contains(constraint.toString().toUpperCase())) {
+                        newFriendsList.add(f);
+                    }
+                }
+                results.values = newFriendsList;
+                results.count = newFriendsList.size();
+            }
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            if (results.count == 0) {
+                mAdapter.notifyDataSetInvalidated();
+            } else {
+                mAdapter.clear();
+                myFriends = (ArrayList<Friend>)results.values;
+                mAdapter.addAll(myFriends);
+                mAdapter.notifyDataSetChanged();
+            }
+        }
+    }*/
+
+
+    /*@Override
+    public View getView(final int position, View convertView, ViewGroup parent) {
+        final Friend friend = getItem(position);
+        ViewHolder viewHolder;
+
+        if (convertView == null) {
+            viewHolder = new ViewHolder();
+
+            convertView = LayoutInflater.from(getContext()).inflate(R.layout.friend_list_item, parent, false);
+
+            viewHolder.avatar = (ImageView) convertView.findViewById(R.id.avatar);
+            viewHolder.name = (TextView) convertView.findViewById(R.id.name);
+            viewHolder.progress = (ProgressBar) convertView.findViewById(R.id.progress);
+            convertView.setTag(viewHolder);
+        } else {
+            viewHolder = (ViewHolder) convertView.getTag();
+        }
+
+        viewHolder.name.setText(friend.getDisplay_name());
+        viewHolder.position = position;
+        new AsyncTask<ViewHolder, Void, Bitmap>() {
+            private ViewHolder v;
+
+            @Override
+            protected void onPostExecute(Bitmap bitmap) {
+                super.onPostExecute(bitmap);
+                if (v.position == position) {
+                    v.progress.setVisibility(View.GONE);
+                    v.avatar.setVisibility(View.VISIBLE);
+                    v.avatar.setImageBitmap(bitmap);
+                }
+            }
+
+            @Override
+            protected Bitmap doInBackground(ViewHolder... params) {
+                v = params[0];
+                String url = friend.getAvatar();
+                Bitmap image = null;
+                try {
+                    InputStream in = new URL(url).openStream();
+                    image = BitmapFactory.decodeStream(in);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                return image;
+            }
+        }.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, viewHolder);
+        return convertView;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new FriendFilter();
+    }*/
